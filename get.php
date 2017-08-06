c5ca37bfbfbca042b6702c1433b95d564a4d2357
NAGARAJ SRJ <nnagarajece31@gmail.com>
2017-08-06 23:37:52 +0530

    Initial Commit

---
+++ get.php
@@ -0,0 +1,5 @@
+<?php
+
+      echo "This is get message from PHP";
+
+?>
---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+
+      $name = $_POST['name'];
+      $id = $_POST['id'];
+      
+      echo "received name is ". $name . " and id is ". $id;
+
+?>
